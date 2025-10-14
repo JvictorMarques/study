0a5044a3fa8e757c980f2acc5b26f366466285f1
JvictorMarques <jvictorsilvamarques358@gmail.com>
2025-10-14 12:18:57 -0300

    feat: implement database and Redis connection functions, update health check endpoints, and add license file

--- backend/main.py
+++ backend/main.py
@@ -4,6 +4,7 @@ from datetime import datetime, timezone
 from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import JSONResponse
+from github import Github
 import redis
 import psycopg2
 
@@ -16,23 +17,46 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+g = Github()
+repo = g.get_repo("jvictormarques/k8s-study")
 
 
-@app.get("/ready")
-def readiness_probe():
-    return {"status": "ready"}
+def get_db_connection():
+    conn = psycopg2.connect(
+        dbname=os.environ["DB_NAME"],
+        user=os.environ["DB_USER"],
+        password=os.environ["DB_PASSWORD"],
+        host=os.environ["DB_HOST"],
+        port=os.environ["DB_PORT"]
+    )
+    return conn
+
+def get_redis_connection():
+    r = redis.Redis(
+        host=os.environ["REDIS_HOST"],
+        port=int(os.environ["REDIS_PORT"]),
+        decode_responses=True
+    )
+    return r
+
+
+@app.get("/health")
+def liveness_probe():
+    return {"status": "healthy"}
+
 
 @app.get("/")
 def root():
     return {"message": "Hello from FastAPI 🚀"}
 
 
-@app.get("/health")
-def healthcheck():
-    health_status = {
-        "status": "healthy",
+@app.get("/ready")
+def readiness_probe():
+
+    ready_status = {
+        "status": "ready",
         "timestamp": datetime.now(timezone.utc).isoformat(),
-        "version": "1.0.0",
+        "version": repo.get_latest_release().tag_name,
         "uptime": "Running",
         "services": {
             "database": "unknown",
@@ -40,69 +64,53 @@ def healthcheck():
         },
         "issues": []
     }
-    
     try:
-        conn = psycopg2.connect(
-            dbname=os.environ["DB_NAME"],
-            user=os.environ["DB_USER"],
-            password=os.environ["DB_PASSWORD"],
-            host=os.environ["DB_HOST"],
-            port=os.environ["DB_PORT"]
-        )
+        conn = get_db_connection()
         conn.close()
-        health_status["services"]["database"] = "healthy"
+        ready_status["services"]["database"] = "ready"
     except Exception as e:
-        health_status["services"]["database"] = "unhealthy"
-        health_status["status"] = "unhealthy"
-        health_status["issues"].append({
+        ready_status["services"]["database"] = "unready"
+        ready_status["status"] = "unready"
+        ready_status["issues"].append({
             "service": "database",
             "message": "Falha na conexão com PostgreSQL",
             "error": str(e)
         })
-    
     try:
-        r = redis.Redis(
-            host=os.environ["REDIS_HOST"],
-            port=int(os.environ["REDIS_PORT"]),
-            decode_responses=True
-        )
+        r = get_redis_connection()
         r.ping()
-        health_status["services"]["cache"] = "healthy"
+        ready_status["services"]["cache"] = "ready"
     except Exception as e:
-        health_status["services"]["cache"] = "unhealthy"
-        health_status["status"] = "unhealthy"
-        health_status["issues"].append({
+        ready_status["services"]["cache"] = "unready"
+        ready_status["status"] = "unready"
+        ready_status["issues"].append({
             "service": "cache",
             "message": "Falha na conexão com Redis",
             "error": str(e)
         })
-    
-    if health_status["status"] == "unhealthy":
+    if ready_status["status"] != "ready":
         return JSONResponse(
             status_code=503,
-            content=health_status
+            content=ready_status
         )
-    
-    return health_status
+    return ready_status
 
 
 @app.get("/db-check")
 def db_check():
     try:
-        conn = psycopg2.connect(
-            dbname=os.environ["DB_NAME"],
-            user=os.environ["DB_USER"],
-            password=os.environ["DB_PASSWORD"],
-            host=os.environ["DB_HOST"],
-            port=os.environ["DB_PORT"]
-        )
+        conn = get_db_connection()
         conn.close()
-        return {"status": "✅ DB conectado", "service": "database", "healthy": True}
+        return {
+            "status": "Database conectado",
+            "service": "database",
+            "healthy": True
+        }
     except Exception as e:
         return JSONResponse(
             status_code=503,
             content={
-                "status": "❌ Erro DB", 
+                "status": "Erro ao se conectar ao banco de dados",
                 "service": "database",
                 "healthy": False,
                 "error": str(e)
@@ -113,15 +121,11 @@ def db_check():
 @app.get("/cache-check")
 def cache_check():
     try:
-        r = redis.Redis(
-            host=os.environ["REDIS_HOST"],
-            port=int(os.environ["REDIS_PORT"]),
-            decode_responses=True
-        )
+        r = get_redis_connection()
         r.set("teste", "ok")
         result = r.get("teste")
         return {
-            "status": "✅ Redis conectado", 
+            "status": "Redis conectado", 
             "service": "cache",
             "healthy": True,
             "value": result
@@ -130,7 +134,7 @@ def cache_check():
         return JSONResponse(
             status_code=503,
             content={
-                "status": "❌ Erro Redis", 
+                "status": "Erro ao se conectar ao Redis",
                 "service": "cache",
                 "healthy": False,
                 "error": str(e)
